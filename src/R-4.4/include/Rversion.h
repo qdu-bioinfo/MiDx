cf0777193323c0df29e3efd541364c4c10abc2b3
Sunyy <74221639+qdu-bioinfo@users.noreply.github.com>
2025-08-08 01:10:08 +0800

    New

---
+++ src/R-4.4/include/Rversion.h
@@ -0,0 +1,29 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 263171
+#define R_NICK "Trophy Case"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "4"
+#define R_MINOR  "4.3"
+#define R_STATUS ""
+#define R_YEAR   "2025"
+#define R_MONTH  "02"
+#define R_DAY    "28"
+#define R_SVN_REVISION 87843
+#ifdef __llvm__
+# define R_FILEVERSION    4,43,22307,0
+#else
+# define R_FILEVERSION    4,43,87843,0
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
